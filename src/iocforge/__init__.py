41e69704bec4a28336618261245a6785e990c66a
Adham Ayman <64089361+Adham504@users.noreply.github.com>
2026-06-28 23:01:36 +0300

    Update author information in __init__.py

--- src/iocforge/__init__.py
+++ src/iocforge/__init__.py
@@ -6,7 +6,7 @@ and reporting Indicators of Compromise (IoCs) from a wide range of file formats.
 
 __title__ = "iocforge"
 __version__ = "1.0.0"
-__author__ = "Your Name <you@example.com>"
+__author__ = Adham Ayman "<adhamayman41212@gmail.com>"
 __license__ = "MIT"
 
 __all__ = ["__version__", "__author__", "__title__", "__license__"]
